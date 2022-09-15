46195090b8dda97b50aac5dc0bf00b2c8ba149e4
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-09-15 19:53:22 -0400

    automate building and creating resume lin

---
+++ buildresume.py
@@ -0,0 +1,18 @@
+import shutil
+import datetime
+import git
+
+repo = git.Repo(search_parent_directories=True)
+branch = repo.active_branch
+
+print("creating resume pdf now")
+
+yr = datetime.datetime.now().year
+original = r'main.pdf'
+
+target = r'UMANG GUPTA RESUME ' + str(yr)
+
+shutil.copyfile(original, target)
+link = 'https://github.com/Usgupta/Umang-CV/blob/'+branch.name+'/'+target
+
+print("resume pdf created, here is the link: " + link)
